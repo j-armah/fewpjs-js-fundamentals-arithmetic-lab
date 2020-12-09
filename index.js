396601ff7654ea2f7f5705d39de6011979693004
februar-y <jarmah241@gmail.com>
2020-12-09 15:13:37 -0500

    done

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+const num1 = 2
+const num2 = 31
+
+var multiply = (num1 * num2);
+
+var random = Math.floor(Math.random() * 10) + 1;
+
+var mod = 10 % 6
+
+var max = Math.max(1,2,3,4,20);
